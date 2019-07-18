2d0d6135e50ddc84f6ab9ca4c3f93733175488c2
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-07-17 22:55:18 -0400

    create firebase app

---
+++ assets/javascript/train.js
@@ -0,0 +1,11 @@
+
+
+var config = {
+    apiKey: "AIzaSyBG5yq90YwpP8JB8IOC-vw_VXibv85y5vw",
+    authDomain: "trainsch-38078.firebaseapp.com",
+    databaseURL: "https://trainsch-38078.firebaseio.com/",
+    projectId: "trainsch-38078",
+    storageBucket: "trainsch-38078.appspot.com",
+    messagingSenderId: "865454381026",
+    appId: "1:865454381026:web:2c29872bbec6cd23"
+  };
